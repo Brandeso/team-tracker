a33e641bbfd081d49581d84a0d5a843e2bca7cfe
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-02-06 19:15:35 -0600

    #Dev - created match component and interface

---
+++ src/app/interfaces/match.ts
@@ -0,0 +1,8 @@
+export interface Match {
+  id: string,
+  localTeam: string,
+  awayTeam: string,
+  localGoals: number,
+  awayGoals: number,
+  date: Date,
+}
---
+++ src/app/match-info/match-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './match-info.component.html',
+  styleUrl: './match-info.component.css'
+})
+export class MatchInfoComponent {
+
+}
