20bcb08d8298e5430ba047cad213cc7e8a011738
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-02-07 00:37:06 -0600

    #Dev - Added angular material

--- src/app/interfaces/match.ts
+++ src/app/interfaces/match.ts
@@ -1,11 +1,14 @@
 export interface Match {
   id: string,
   data: {
+    season?: string,
     localTeam?: string,
     awayTeam?: string,
     localGoals?: number,
     awayGoals?: number,
     date?: Date,
+    squad?: String[],
+    scorers?: { name: string, goals: number }[],
     deleted: boolean
   }
 }
