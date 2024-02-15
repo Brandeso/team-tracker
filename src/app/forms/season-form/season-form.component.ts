05483cf978afb5241a5433dede4a239bf2990bb8
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-02-15 00:07:34 -0600

    Added season form component

---
+++ src/app/forms/season-form/season-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-season-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './season-form.component.html',
+  styleUrl: './season-form.component.css'
+})
+export class SeasonFormComponent {
+
+}
